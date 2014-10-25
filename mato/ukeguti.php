87f3969215e53c8def43c893f01e77e374f221bc
diamond2000 <kwsk9849:@yahoo.co.jp>
2014-10-25 17:24:41 +0900

    aa

--- mato/ukeguti.php
+++ mato/ukeguti.php
@@ -7,16 +7,18 @@
 <script src="javascript-xpath-latest.js"></script>
 <?php
 require( "common.php" );
+include_once('simple_html_dom.php');
 ?>
 
 
 <?php
 
 $cstrsiteHTML = pfHpGet($_GET['name']);
+$html = str_get_html($cstrsiteHTML);
 
 //XpathでDOM要素の指定をする　なお、ループ処理になるのでforeachを使う  
-  foreach($cstrsiteHTM->find('/html/body/div[3]/article/nav/ul/li[2]/ul/li[3] a') as $element){  
-  echo $element->href . '<br>';  
+  foreach($html->find('/html/body/div[3]/article/nav/ul/li[2]/ul/li a') as $element){  
+  echo $element->href ;
   }  
 
 ?>
