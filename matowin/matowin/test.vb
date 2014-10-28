7d65a60c2de456693431719a4f5db7ece3d249f8
dia <kwsk9849kwsk@yahoo.co.jp>
2014-10-28 20:05:48 +0900

    レス色変え機能削除

--- matowin/matowin/test.vb
+++ matowin/matowin/test.vb
@@ -3,13 +3,15 @@
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
         Try
             Dim lstrtitle As String
-            Dim lstrBody As String
+            Dim larray As ArrayList
 
             Dim lcls As New resufurikae
 
-            lcls.shutoku("", lstrtitle, lstrBody)
+            lcls.shutoku("http://viper.2ch.sc/test/read.cgi/news4vip/1414438839/", lstrtitle, larray)
             RichTextBox1.Text += lstrtitle & vbCrLf
-            RichTextBox1.Text += lstrBody
+            For i As Integer = 0 To larray.Count - 1
+                RichTextBox1.AppendText(larray(i).ToString)
+            Next
         Catch ex As Exception
             MsgBox(ex.ToString)
         End Try
