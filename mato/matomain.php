dac1d447ac197fea2f6e3616851e7f96ee76b7f4
dia <kwsk9849kwsk@yahoo.co.jp>
2014-10-30 19:28:53 +0900

    anchar修正

---
+++ mato/matomain.php
@@ -0,0 +1,49 @@
+﻿<html>
+<head>
+<title>mato</title>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	
+</head>
+<body>
+<script src="javascript-xpath-latest.js"></script>
+<?php
+
+//ASP経由で振り分け取得
+function getFuriwakeHTML(){
+	//引数セット
+	$data = array(
+	    'str' => 'aa',
+	);
+
+	$context = stream_context_create(array('http' => array(
+	'method' => 'POST',
+	'header' => 'User-Agent: Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)',
+	'content' => http_build_query($data),
+	)));
+	$url = "http://localhost:2405/Service1.asmx/matotest";
+
+	$html = file_get_contents($url, false, $context);
+        return $html;
+}
+
+//本文格納
+function getHONBUNHTML($html){
+	$honbunhtml = str_get_html($html);
+	return $honbunhtml;
+}
+?>
+
+<form action="" method="POST" style="width:100%;text-align:center">
+Webデータ取得<br>
+<textarea cols="80" rows="20">
+
+<?php
+
+	echo getFuriwakeHTML();                                //Webデータ表示
+?>
+
+</textarea>
+
+</form>
+</body>
+</html>
---
+++ mato/matomain2.php
@@ -0,0 +1,71 @@
+﻿<html>
+<head>
+<title>まとめ</title>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+</head>
+<body>
+<script src="javascript-xpath-latest.js"></script>
+<?php
+require( "common.php" );
+include_once('simple_html_dom.php');
+
+//ASP経由で振り分け取得
+function getFuriwakeHTML(){
+	//引数セット
+	$data = array(
+	    'str' => 'aa',
+	);
+	$headers = array(
+	    'Content-Type: application/x-www-form-urlencoded',
+	    'Content-Length: '.strlen(http_build_query($data))
+	        );
+
+
+	$context = stream_context_create(array('http' => array(
+	'method' => 'POST',
+	'header' => implode("\r\n", $headers),
+	'content' => http_build_query($data),
+	)));
+	$url = "http://localhost:2405/Service1.asmx/matotest";
+
+	$html = file_get_contents($url, false, $context);
+        return htmlspecialchars_decode ($html);
+}
+
+//本文格納
+function getHONBUNHTML($html){
+	$honbunhtml = str_get_html($html);
+	return $honbunhtml;
+}
+?>
+
+<form action="" method="POST" style="width:100%;text-align:center">
+Webデータ取得<br>
+<textarea cols="80" rows="20">
+<?php
+	//レス振り分け
+	//$cstrsiteHTML = "<html><head></head><body>" . getFuriwakeHTML() ."</body></html>";
+	$cstrsiteHTML =  getFuriwakeHTML() ;
+?>
+</textarea>
+<?php
+	$string = getFuriwakeHTML();
+	//アンカー置き換え
+	$pattern= '/(<a href)(.*?)(&gt;&gt;)/is';
+	$string = preg_replace($pattern, '<span style="color:mediumblue;" class="anchor">&gt;&gt;', $string);
+	$pattern= '/&gt;&gt;(.+?)<\/a>/';
+	$string = preg_replace($pattern, '&gt;&gt;$1</span>', $string);
+	//BR置き換え
+	$pattern= '/<br><br>/';
+	$string = preg_replace($pattern, '<br />', $string);
+	$pattern= '/<br>/';
+	$string = preg_replace($pattern, '<br />', $string);
+
+	//アレイリスト
+	$pattern= '/(<dt>)(.*?)(<\/dt>)/is';
+	preg_match_all($pattern, $string , $match);
+	print_r($match);
+?>
+
+</body>
+</html>
