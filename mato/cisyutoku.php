7d6ab5b35c03a39a5e635ad80320990970c0021e
dia <kwsk9849kwsk@yahoo.co.jp>
2014-11-03 06:14:19 +0900

    form

--- mato/cisyutoku.php
+++ mato/cisyutoku.php
@@ -4,10 +4,34 @@ include_once('simple_html_dom.php');
 $cstrsiteHTML = pfHpGet('http://2ch-c.net/');
 $html = str_get_html($cstrsiteHTML);
 
+
 //XpathでDOM要素の指定をする　なお、ループ処理になるのでforeachを使う  
   foreach($html->find('div[class="bar"]') as $element){  
-  	echo $element->parent->parent->parent->innertext ."</br>"."\n";
-//print_r  ($element->parent);
+	if ($element->innertext > 500){
+
+		//まとめブログ取得
+		$matomehtml=str_get_html($element->parent->parent->parent->innertext);
+		$cnt=0;
+		foreach($matomehtml->find('a') as $matomeelement){
+			if ($cnt==0){
+				$cstrSAKIHTML = pfHpGet($matomeelement->href);
+				if(preg_match_all('(https?://[-_.!~*\'()a-zA-Z0-9;/?:@&=+$,%#]+)', $cstrSAKIHTML, $result) !== false){
+				    foreach ($result[0] as $value){
+				        //URL表示
+				        	//print $value . 'AAAA<br>';
+					if (strpos($value, "2ch.sc") !== FALSE){
+						print '-----------'. '<br>';
+						echo $element->parent->parent->parent->innertext ."</br>"."\n";
+				        	print $value . '<br>';
+						break;
+					}
+
+				    }
+				}
+			}
+			$cnt=$cnt+1;
+		}
+	}
   }  
 
 //  echo $html ;
