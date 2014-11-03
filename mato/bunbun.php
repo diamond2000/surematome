25b29e0fd635a21377d4c561e43c80bbfd7f44f8
dia <kwsk9849kwsk@yahoo.co.jp>
2014-11-03 10:32:44 +0900

    aa

---
+++ mato/bunbun.php
@@ -0,0 +1,29 @@
+﻿<html>
+<head>
+<title>ぶんぶんまとめ</title>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+</head>
+<body>
+<?php
+include_once('matookikae.php');
+include_once('simple_html_dom.php');
+
+	$match = getmatookikae($_GET["url"]);
+
+
+	//画像置き換え
+	$count = 0;
+	while ($count < count($match[0])){
+		$pattern= '/<a href="(.*?).jpg" target="_blank">(.*?)<\/a>/';
+		$match[0][$count] = preg_replace($pattern, '<a href="$1.jpg"><img src="$1.jpg">$2</img></a>' , $match[0][$count]);
+		$pattern= '/<a href="(.*?).png" target="_blank">(.*?)<\/a>/';
+		$match[0][$count] = preg_replace($pattern, '<a href="$1.png"><img src="$1.png">$2</img></a>' , $match[0][$count]);
+		$pattern= '/<a href="(.*?).gif" target="_blank">(.*?)<\/a>/';
+		$match[0][$count] = preg_replace($pattern, '<a href="$1.gif"><img src="$1.gif">$2</img></a>' , $match[0][$count]);
+	    $count=$count+1;
+	}
+	print_r($match);
+?>
+
+</body>
+</html>
--- mato/cisyutoku.php
+++ mato/cisyutoku.php
@@ -1,15 +1,15 @@
-<?php
+ï»¿<?php
 require( "common.php" );
 include_once('simple_html_dom.php');
 $cstrsiteHTML = pfHpGet('http://2ch-c.net/');
 $html = str_get_html($cstrsiteHTML);
 
 
-//Xpath‚ÅDOM—v‘f‚ÌŽw’è‚ð‚·‚é@‚È‚¨Aƒ‹[ƒvˆ—‚É‚È‚é‚Ì‚Åforeach‚ðŽg‚¤  
+//Xpathã§DOMè¦ç´ ã®æŒ‡å®šã‚’ã™ã‚‹ã€€ãªãŠã€ãƒ«ãƒ¼ãƒ—å‡¦ç†ã«ãªã‚‹ã®ã§foreachã‚’ä½¿ã†  
   foreach($html->find('div[class="bar"]') as $element){  
 	if ($element->innertext > 500){
 
-		//‚Ü‚Æ‚ßƒuƒƒOŽæ“¾
+		//ã¾ã¨ã‚ãƒ–ãƒ­ã‚°å–å¾—
 		$matomehtml=str_get_html($element->parent->parent->parent->innertext);
 		$cnt=0;
 		foreach($matomehtml->find('a') as $matomeelement){
@@ -17,12 +17,14 @@ $html = str_get_html($cstrsiteHTML);
 				$cstrSAKIHTML = pfHpGet($matomeelement->href);
 				if(preg_match_all('(https?://[-_.!~*\'()a-zA-Z0-9;/?:@&=+$,%#]+)', $cstrSAKIHTML, $result) !== false){
 				    foreach ($result[0] as $value){
-				        //URL•\Ž¦
+				        //URLè¡¨ç¤º
 				        	//print $value . 'AAAA<br>';
 					if (strpos($value, "2ch.sc") !== FALSE){
-						print '-----------'. '<br>';
+						print '-----------'. '<br>'."\n";
 						echo $element->parent->parent->parent->innertext ."</br>"."\n";
-				        	print $value . '<br>';
+				        	print $value . '<br>'."\n";
+						//æŠ•ç¨¿ç”¨ãƒªãƒ³ã‚¯
+						echo '<a href="http://127.0.0.1/project/surematome3/surematome/mato/bunbun.php?url='.$value.'">ãƒ–ãƒ³ãƒ–ãƒ³</a></br>'."\n";
 						break;
 					}
 
