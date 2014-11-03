0e4cb1a8b0bdecb23abd16a8f9c233fcb4fe373a
dia <kwsk9849kwsk@yahoo.co.jp>
2014-11-03 12:36:20 +0900

    bunbun画像まで

--- mato/bunbun.php
+++ mato/bunbun.php
@@ -14,15 +14,31 @@ include_once('simple_html_dom.php');
 	//画像置き換え
 	$count = 0;
 	while ($count < count($match[0])){
-		$pattern= '/<a href="(.*?).jpg" target="_blank">(.*?)<\/a>/';
-		$match[0][$count] = preg_replace($pattern, '<a href="$1.jpg"><img src="$1.jpg">$2</img></a>' , $match[0][$count]);
-		$pattern= '/<a href="(.*?).png" target="_blank">(.*?)<\/a>/';
-		$match[0][$count] = preg_replace($pattern, '<a href="$1.png"><img src="$1.png">$2</img></a>' , $match[0][$count]);
-		$pattern= '/<a href="(.*?).gif" target="_blank">(.*?)<\/a>/';
-		$match[0][$count] = preg_replace($pattern, '<a href="$1.gif"><img src="$1.gif">$2</img></a>' , $match[0][$count]);
+		//$pattern= '/<a href="(.*?).jpg" target="_blank">(.*?)<\/a>/';
+		//$match[0][$count] = preg_replace($pattern, '<a href="$1.jpg"><img src="$1.jpg">$2</img></a>' , $match[0][$count]);
+		//$pattern= '/<a href="(.*?).png" target="_blank">(.*?)<\/a>/';
+		//$match[0][$count] = preg_replace($pattern, '<a href="$1.png"><img src="$1.png">$2</img></a>' , $match[0][$count]);
+		//$pattern= '/<a href="(.*?).gif" target="_blank">(.*?)<\/a>/';
+		//$match[0][$count] = preg_replace($pattern, '<a href="$1.gif"><img src="$1.gif">$2</img></a>' , $match[0][$count]);
+
+		echo $match[0][$count];
+
+
+		$pattern = '/(https?)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)\.(jpg|gif|png)/';
+		preg_match_all($pattern, $match[0][$count], $matches);
+		if(count($matches)){
+		    foreach($matches[0] as $i=>$url) {
+		        //$replace = "<img src='h{$url}'>";
+		        //$images[] = $replace;
+		        //$body = str_replace($url, $replace, $body);
+			echo '<img src="http://127.0.0.1/project/surematome3/surematome/mato/gazosyori.php?url='.$url.'"></br>';
+		    }
+		}
+
+
 	    $count=$count+1;
 	}
-	print_r($match);
+	//print_r($match);
 ?>
 
 </body>
