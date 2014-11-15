0f1bd9b505f697ca293d1238d8642c7c1dfc6b79
dia <kwsk9849kwsk@yahoo.co.jp>
2014-11-15 21:22:35 +0900

    aa

--- mato/test.php
+++
@@ -1,5 +0,0 @@
-﻿<?php
-$doc = new DOMDocument();
-$doc->loadHTML('<html><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /><body>aaaああ<br></body></html>');
-echo $doc->saveHTML();
-?>
